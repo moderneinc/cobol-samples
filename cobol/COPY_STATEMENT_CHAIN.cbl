a723b9aca794cd111f19517e4ff5b91f4ac95427
traceyyoshima <tracey.yoshima@gmail.com>
2023-08-16 17:40:23 -0600

    Added example for a chain of copy statements.

---
+++ cobol/COPY_STATEMENT_CHAIN.cbl
@@ -0,0 +1,6 @@
+000000 IDENTIFICATION DIVISION.                                         *
+       PROGRAM-ID. COPY_STATEMENT_CHAIN.                                *
+       DATA DIVISION.                                                   *
+       LINKAGE SECTION.                                                 *
+           01  GRP-01.                                                  *
+               COPY INCEPTION.                                          *
