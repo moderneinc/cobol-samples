c4cb7b2d4c93a03f5a9381d6e782aaf1600356b3
traceyyoshima <tracey.yoshima@gmail.com>
2023-08-22 15:46:20 -0600

    Fixed program name in COPY_STATEMENT_CHAIN_2

--- cobol/COPY_STATEMENT_CHAIN_2.cbl
+++ cobol/COPY_STATEMENT_CHAIN_2.cbl
@@ -1,7 +1,7 @@
       * This is identical to COPY_STATEMENT_CHAIN to show the
       * relationship between chains is not duplicated.
 000000 IDENTIFICATION DIVISION.                                         *
-       PROGRAM-ID. COPY_STATEMENT_CHAIN.                                *
+       PROGRAM-ID. COPY_STATEMENT_CHAIN_2.                              *
        DATA DIVISION.                                                   *
        LINKAGE SECTION.                                                 *
            01  GRP-01.                                                  *
