da259f41e74f2c8a89a5c937e569ad48e41e6946
traceyyoshima <tracey.yoshima@gmail.com>
2023-08-22 15:30:09 -0600

    Added examples to show that redundant copybook relationships are not rendered on the SAAS.

---
+++ cobol/COPY_STATEMENT_CHAIN_2.cbl
@@ -0,0 +1,8 @@
+      * This is identical to COPY_STATEMENT_CHAIN to show the
+      * relationship between chains is not duplicated.
+000000 IDENTIFICATION DIVISION.                                         *
+       PROGRAM-ID. COPY_STATEMENT_CHAIN.                                *
+       DATA DIVISION.                                                   *
+       LINKAGE SECTION.                                                 *
+           01  GRP-01.                                                  *
+               COPY INCEPTION.                                          *
